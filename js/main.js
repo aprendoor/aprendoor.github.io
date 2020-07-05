26c96b70142408c9eb9fd3ab1da6eb2dcf565491
aprendoor <67879902+aprendoor@gmail.com>
2020-07-06 04:32:40 +0530

    website initial update

---
+++ js/main.js
@@ -0,0 +1,7 @@
+console.log("This is main.js");
+// slanted div height are :: approximatly Half of the current visible screen
+let mainSlantedDiv = document.querySelector(".main-slanted-div");
+mainSlantedDiv.style.minHeight = window.innerHeight/2 +80 + "px";
+
+
+
